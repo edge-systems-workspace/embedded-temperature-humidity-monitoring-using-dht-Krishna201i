bbfb2a996a45987138ebe5bbf93f36f06d212428
krishna201i <krishna.singh_cs.aiml24@gla.ac.in>
2026-02-19 23:21:34 +0530

    remove completed TODO comments for DHT sensor setup

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,6 @@
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-
-// TODO 1:
-// Define the DHT data pin (Use digital pin 2)
-
-// TODO 2:
-// Define the DHT sensor type (DHT11)
-
-// TODO 3:
-// Create a DHT object using the defined pin and sensor type
-
 void setup() {
 
     // TODO 4:
